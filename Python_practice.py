96ba7e0b306232c79640d25efc30350ca6add1eb
scrussell2 <stevencrussell2@gmail.com>
2022-11-12 20:24:25 -0600

    Adding PyPoll.py file.

---
+++ Python_practice.py
@@ -0,0 +1,18 @@
+counties = ["Arapahoe","Denver","Jefferson"]
+if "El Paso" in counties:
+    print("El Paso is in the list of counties.")
+else:
+    print("El Paso is not in the list of counties")
+
+if "Arapahoe" in counties or "El Paso" in counties:
+    print("Arapahoe and El Paso are in the list of counties.")
+else:
+    print("Arapaho or El Paso is not in the list of counties")
+if "Arapahoe" in counties and "El Paso" not in counties:
+   print("Only Arapahoe is in the list of counties.")
+else:
+    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+
+for county in counties:
+    print(county)
+    
